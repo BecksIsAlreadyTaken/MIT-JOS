cfdd62ea57604662e129262c4f1446fe0de6abc3
beck <503646976@qq.com>
2017-11-15 23:11:45 +0800

    Lab1Exercise11&12

--- kern/monitor.c
+++ kern/monitor.c
@@ -58,17 +58,20 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+	struct Eipdebuginfo eipinfo;
 	uint32_t ebp=read_ebp();
 	uint32_t eip,args[5];
 	cprintf("Stack backtrace:\n");
 	while(ebp){
 		eip=*((uint32_t *)ebp+1);
+		debuginfo_eip(eip,&eipinfo);
 		args[0]=*((uint32_t *)ebp+2);
 		args[1]=*((uint32_t *)ebp+3);
 		args[2]=*((uint32_t *)ebp+4);
 		args[3]=*((uint32_t *)ebp+5);
 		args[4]=*((uint32_t *)ebp+6);
 		cprintf("  ebp %08x eip %08x args %08x %08x %08x %08x %08x\n",ebp,eip,args[0],args[1],args[2],args[3],args[4]);
+		cprintf("         %s:%d: %.*s+%d\n", eipinfo.eip_file, eipinfo.eip_line, eipinfo.eip_fn_namelen, eipinfo.eip_fn_name, eip - eipinfo.eip_fn_addr);
 		ebp=*(uint32_t *)ebp;
 	}
 	return 0;
